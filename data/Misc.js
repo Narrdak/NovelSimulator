c20027f5f0f33cf318638f639b8c66afa8e9fd36
Narrdak <narrchy@gmail.com>
2025-06-14 21:30:35 +0900

    Add files via upload

--- data/Misc.js
+++ data/Misc.js
@@ -3,5 +3,122 @@
  * @file 이 파일은 랭킹 티어 기준 등 기타 분류에 속하지 않는 게임 데이터를 포함합니다.
  */
 
-export const rankingTiers = [ { views: 12000, rank: '1위' }, { views: 10000, rank: '2위' }, { views: 9000,  rank: '3위' }, { views: 8000,  rank: '4위' }, { views: 7000,  rank: '5위' }, { views: 6500,  rank: '6위' }, { views: 6000,  rank: '7위' }, { views: 5700,  rank: '8위' }, { views: 5500,  rank: '9위' }, { views: 5300,  rank: '10위' }, { views: 5000,  rank: '11위' }, { views: 4889,  rank: '12위' }, { views: 4778,  rank: '13위' }, { views: 4667,  rank: '14위' }, { views: 4556,  rank: '15위' }, { views: 4444,  rank: '16위' }, { views: 4333,  rank: '17위' }, { views: 4222,  rank: '18위' }, { views: 4111,  rank: '19위' }, { views: 4000,  rank: '20위' }, { views: 3889,  rank: '21위' }, { views: 3778,  rank: '22위' }, { views: 3667,  rank: '23위' }, { views: 3556,  rank: '24위' }, { views: 3444,  rank: '25위' }, { views: 3333,  rank: '26위' }, { views: 3222,  rank: '27위' }, { views: 3111,  rank: '28위' }, { views: 3000,  rank: '30위' }, { views: 2944,  rank: '31위' }, { views: 2889,  rank: '32위' }, { views: 2833,  rank: '33위' }, { views: 2778,  rank: '34위' }, { views: 2722,  rank: '35위' }, { views: 2667,  rank: '36위' }, { views: 2611,  rank: '37위' }, { views: 2556,  rank: '38위' }, { views: 2500,  rank: '40위' }, { views: 2444,  rank: '41위' }, { views: 2389,  rank: '42위' }, { views: 2333,  rank: '43위' }, { views: 2278,  rank: '44위' }, { views: 2222,  rank: '45위' }, { views: 2167,  rank: '46위' }, { views: 2111,  rank: '47위' }, { views: 2056,  rank: '48위' }, { views: 2000,  rank: '50위' }, { views: 1974,  rank: '51위' }, { views: 1947,  rank: '52위' }, { views: 1921,  rank: '53위' }, { views: 1895,  rank: '54위' }, { views: 1868,  rank: '55위' }, { views: 1842,  rank: '56위' }, { views: 1816,  rank: '57위' }, { views: 1789,  rank: '58위' }, { views: 1763,  rank: '59위' }, { views: 1737,  rank: '60위' }, { views: 1711,  rank: '61위' }, { views: 1684,  rank: '62위' }, { views: 1658,  rank: '63위' }, { views: 1632,  rank: '64위' }, { views: 1605,  rank: '65위' }, { views: 1579,  rank: '66위' }, { views: 1553,  rank: '67위' }, { views: 1526,  rank: '68위' }, { views: 1500,  rank: '70위' }, { views: 1476,  rank: '71위' }, { views: 1452,  rank: '72위' }, { views: 1428,  rank: '73위' }, { views: 1404,  rank: '74위' }, { views: 1379,  rank: '75위' }, { views: 1355,  rank: '76위' }, { views: 1331,  rank: '77위' }, { views: 1307,  rank: '78위' }, { views: 1283,  rank: '79위' }, { views: 1259,  rank: '80위' }, { views: 1234,  rank: '81위' }, { views: 1210,  rank: '82위' }, { views: 1186,  rank: '83위' }, { views: 1162,  rank: '84위' }, { views: 1138,  rank: '85위' }, { views: 1114,  rank: '86위' }, { views: 1090,  rank: '87위' }, { views: 1066,  rank: '88위' }, { views: 1041,  rank: '89위' }, { views: 1017,  rank: '90위' }, { views: 993,   rank: '91위' }, { views: 969,   rank: '92위' }, { views: 945,   rank: '93위' }, { views: 921,   rank: '94위' }, { views: 897,   rank: '95위' }, { views: 872,   rank: '96위' }, { views: 848,   rank: '97위' }, { views: 824,   rank: '98위' }, { views: 812,   rank: '99위' }, { views: 800,   rank: '100위' }, { views: 700,   rank: '상위 5%' }, { views: 600,   rank: '상위 10%' }, { views: 400,   rank: '상위 20%' }, { views: 0,     rank: '순위권 밖' } ];
+export const rankingTiers = [
+    // === 최상위 랭킹 (1-10위) ===
+    { views: 12000, rank: '1위' },
+    { views: 10000, rank: '2위' },
+    { views: 9000,  rank: '3위' },
+    { views: 8000,  rank: '4위' },
+    { views: 7000,  rank: '5위' },
+    { views: 6500,  rank: '6위' },
+    { views: 6000,  rank: '7위' },
+    { views: 5700,  rank: '8위' },
+    { views: 5500,  rank: '9위' },
+    { views: 5300,  rank: '10위' },
+  
+    // === 상위 랭킹 (11-30위) ===
+    { views: 5000,  rank: '11위' },
+    { views: 4889,  rank: '12위' },
+    { views: 4778,  rank: '13위' },
+    { views: 4667,  rank: '14위' },
+    { views: 4556,  rank: '15위' },
+    { views: 4444,  rank: '16위' },
+    { views: 4333,  rank: '17위' },
+    { views: 4222,  rank: '18위' },
+    { views: 4111,  rank: '19위' },
+    { views: 4000,  rank: '20위' },
+    { views: 3889,  rank: '21위' },
+    { views: 3778,  rank: '22위' },
+    { views: 3667,  rank: '23위' },
+    { views: 3556,  rank: '24위' },
+    { views: 3444,  rank: '25위' },
+    { views: 3333,  rank: '26위' },
+    { views: 3222,  rank: '27위' },
+    { views: 3111,  rank: '28위' },
+    { views: 3050,  rank: '29위' },  // 빠진 순위 추가
+    { views: 3000,  rank: '30위' },
+  
+    // === 중상위 랭킹 (31-50위) ===
+    { views: 2944,  rank: '31위' },
+    { views: 2889,  rank: '32위' },
+    { views: 2833,  rank: '33위' },
+    { views: 2778,  rank: '34위' },
+    { views: 2722,  rank: '35위' },
+    { views: 2667,  rank: '36위' },
+    { views: 2611,  rank: '37위' },
+    { views: 2556,  rank: '38위' },
+    { views: 2528,  rank: '39위' },  // 빠진 순위 추가
+    { views: 2500,  rank: '40위' },
+    { views: 2444,  rank: '41위' },
+    { views: 2389,  rank: '42위' },
+    { views: 2333,  rank: '43위' },
+    { views: 2278,  rank: '44위' },
+    { views: 2222,  rank: '45위' },
+    { views: 2167,  rank: '46위' },
+    { views: 2111,  rank: '47위' },
+    { views: 2056,  rank: '48위' },
+    { views: 2028,  rank: '49위' },  // 빠진 순위 추가
+    { views: 2000,  rank: '50위' },
+  
+    // === 중위 랭킹 (51-70위) ===
+    { views: 1974,  rank: '51위' },
+    { views: 1947,  rank: '52위' },
+    { views: 1921,  rank: '53위' },
+    { views: 1895,  rank: '54위' },
+    { views: 1868,  rank: '55위' },
+    { views: 1842,  rank: '56위' },
+    { views: 1816,  rank: '57위' },
+    { views: 1789,  rank: '58위' },
+    { views: 1763,  rank: '59위' },
+    { views: 1737,  rank: '60위' },
+    { views: 1711,  rank: '61위' },
+    { views: 1684,  rank: '62위' },
+    { views: 1658,  rank: '63위' },
+    { views: 1632,  rank: '64위' },
+    { views: 1605,  rank: '65위' },
+    { views: 1579,  rank: '66위' },
+    { views: 1553,  rank: '67위' },
+    { views: 1526,  rank: '68위' },
+    { views: 1513,  rank: '69위' },  // 빠진 순위 추가
+    { views: 1500,  rank: '70위' },
+  
+    // === 중하위 랭킹 (71-100위) ===
+    { views: 1476,  rank: '71위' },
+    { views: 1452,  rank: '72위' },
+    { views: 1428,  rank: '73위' },
+    { views: 1404,  rank: '74위' },
+    { views: 1379,  rank: '75위' },
+    { views: 1355,  rank: '76위' },
+    { views: 1331,  rank: '77위' },
+    { views: 1307,  rank: '78위' },
+    { views: 1283,  rank: '79위' },
+    { views: 1259,  rank: '80위' },
+    { views: 1234,  rank: '81위' },
+    { views: 1210,  rank: '82위' },
+    { views: 1186,  rank: '83위' },
+    { views: 1162,  rank: '84위' },
+    { views: 1138,  rank: '85위' },
+    { views: 1114,  rank: '86위' },
+    { views: 1090,  rank: '87위' },
+    { views: 1066,  rank: '88위' },
+    { views: 1041,  rank: '89위' },
+    { views: 1017,  rank: '90위' },
+    { views: 993,   rank: '91위' },
+    { views: 969,   rank: '92위' },
+    { views: 945,   rank: '93위' },
+    { views: 921,   rank: '94위' },
+    { views: 897,   rank: '95위' },
+    { views: 872,   rank: '96위' },
+    { views: 848,   rank: '97위' },
+    { views: 824,   rank: '98위' },
+    { views: 812,   rank: '99위' },
+    { views: 800,   rank: '100위' },
+  
+    // === 백분위 기준 ===
+    { views: 700,   rank: '상위 5%' },
+    { views: 600,   rank: '상위 10%' },
+    { views: 400,   rank: '상위 20%' },
+    { views: 0,     rank: '순위권 밖' }
+  ];
+
 // --- END OF FILE data/Misc.js ---
